0b48fe6e48e694a080157d3a0cb6c2dfc74cb318
yeland <wlx100715@163.com>
2018-08-03 14:45:07 +0800

    complete main

--- main/main.js
+++ main/main.js
@@ -1,3 +1,14 @@
 module.exports = function main() {
   // Write your code here
+  if(arguments.length===0){
+	  return 0;
+  }
+  
+  var str = arguments[0];
+  var sum = 0;
+  for(var i=0;i<str.length;i++){
+	  sum = sum + parseInt(str.charAt(i));
+  }
+	  
+  return sum;
 };
